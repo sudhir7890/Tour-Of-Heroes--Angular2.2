63904356ae819d0424cb12be8116b3c43f257349
Sudhir R <srangu81@gmail.com>
2016-12-13 14:16:42 -0600

    resuable component of hero details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
-export class Hero {
-  id : number;
-  name : string;
-}
+
 
 
 @Component({
@@ -18,14 +16,7 @@ export class Hero {
     <span class = "badge">{{hero.id}}</span>{{hero.name}}
     </li>
     </ul>
-    <div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} Details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-  <label>name:</label>
-  <input [(ngModel)]= "selectedHero.name" placeholder = "name">
-  </div> 
-  </div>
+    <my-hero-detail [hero] ="selectedHero"></my-hero-detail>
   `,
   styles: [`
     .selected: {
